d9e5e6ea8889a5b5e8898100398633cf831225c4
name <bgpro714@naver.com>
2024-02-23 17:54:10 +0900

    코드및 주석

--- backend/service/ConfService.js
+++ backend/service/ConfService.js
@@ -1,30 +1,45 @@
-const { connection } = require("../dbConnect.js");
+const { connection } = require("../dbConnect.js"); //databasePool로 
 //const mapper = require("./SqlMapper");
 //const sqlMapper = require("#service/SqlMapper.js");
 //const mapper = new SqlMapper();
-const mapper = require("#service/SqlMapper.js");
+const mapper = require("./SqlMapper.js");
 
 class ConfService {
   constructor(namespace) {
     console.log("constructor: conf");
   }
-  loadNamespace() {
-    mapper.setNamespace("conf");
-  }
 
   async getConfList() {
-    this.loadNamespace();
-    const query = mapper.getSql("selectTreeMenuList");
-
-    const pool = await connection;
-    const result = await pool.request().query(query);
-
-    console.log("result", result);
-
+    const result = this.selectQuery("selectConfList"); //select id 값
     return result;
   }
 
+  loadNamespace() {
+    // 네임스페이스 세팅
+    mapper.setNamespace("conf");
+  }
 
+  async executeQuery(query) {
+    const pool = await connection;
+    const result = await pool.request().query(query);
+    return result;
+  }
+
+  async selectQuery(mapperId) {
+    this.loadNamespace();
+    const query = mapper.getSql(mapperId);
+    const result = await this.executeQuery(query);
+    console.log("selectQuery", query);
+    return result;
+  }
+
+  async selectQueryWithParms(mapperId, params) {
+    this.loadNamespace();
+    const query = mapper.getSql(mapperId, params);
+    const result = await this.executeQuery(query);
+
+    return result;
+  }
 }
 
 module.exports = ConfService;
